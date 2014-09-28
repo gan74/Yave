89809c145b44665a212d82d7689ab3afe7d17d2a
gan74 <gregoire.angerand@epfl.ch>
2014-09-28 16:53:41 +0200

    Emptied main from previous experiment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <n/core/Array.h>
 #include <n/core/String.h>
 #include <n/Types.h>
@@ -11,7 +12,6 @@
 #include <iostream>
 #include <set>
 
-#include <n/mem/Allocator.h>
 
 using namespace n::core;
 using namespace n;
@@ -46,34 +46,6 @@ struct IntFunc
 
 int main(int, char **) {
 	std::cout<<std::boolalpha;
-
-	mem::Allocator<4, 60> alloc;
-	Array<void *> ptrs;
-	uint max = 4096;
-	for(uint i = 0; i != max; i++) {
-
-		void *ptr = 0;
-		ptr = alloc.allocate();
-		ptrs.append(ptr);
-		if(i % 255 == 0 && i) {
-			uint si  = random(ptrs.size() / 2, ptrs.size() / 16);
-			std::cout<<"freeing "<<si<<"/"<<ptrs.size()<<" : "<<std::endl;
-			for(uint w = 0; w != si; w++) {
-				uint in = random(ptrs.size());
-				void *p = ptrs[in];
-				ptrs[in] = ptrs.last();
-				ptrs.pop();
-				alloc.desallocate(p);
-			}
-			std::cout<<"\t"<<ptrs.size()<<" live ptrs : "<<alloc.getChunkCount()<<" chunks ("<<alloc.getChunkCount() * 60<<" blocks)"<<std::endl;
-			std::cout<<"\toptimal = "<<ptrs.size() / 60 + 1<<std::endl<<std::endl;
-		}
-	}
-	std::cout<<"freeing memory...."<<std::endl;
-	for(void *p : ptrs) {
-		alloc.desallocate(p);
-	}
-
 	return 0;
 }
 
