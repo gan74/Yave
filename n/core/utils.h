6728dfd6ad029b09e82bac2b9c8bc4b3affee9b8
gan74 <gregoire.angerand@epfl.ch>
2014-10-04 18:37:40 +0200

    Added radix sort implementation

--- n/core/utils.h
+++ n/core/utils.h
@@ -19,6 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <n/Types.h>
 #include <tgmath.h>
+#include <algorithm>
+
+#include <vector>
 
 namespace n {
 namespace core {
@@ -31,25 +34,9 @@ float random();
 float toDeg(float a);
 float toRad(float a);
 
-union nColor
-{
-	nColor() : id(0) {
-	}
+uint hash(const void *key, uint len, uint seed = 0x1000193);
+uint log2ui(uint n);
 
-	nColor(byte R, byte G, byte B, byte A = 255) : rgba({R, G, B, A}) {
-	}
-
-	struct
-	{
-		byte r;
-		byte g;
-		byte b;
-		byte a;
-	} rgba;
-	uint id;
-	float real;
-	byte bytes[4];
-};
 
 template<typename T>
 const T &clamp(const T &val, const T &mi, const T &ma) {
@@ -66,8 +53,30 @@ T sign(T x) {
 	return (T(0) < x) - (x < T(0));
 }
 
-uint hash(const void *key, uint len, uint seed = 0x1000193);
-uint log2ui(uint n);
+template<typename I>
+void radixSort(I beg, I end) {
+	typedef typename TypeContent<I>::type ElemType;
+	static_assert(std::is_integral<ElemType>::value, "Element type needs to be integral");
+	std::vector<ElemType> buckets[16];
+	for(uint i = 0; i != 2 * sizeof(ElemType); i++) {
+		for(I e = beg; e != end; ++e) {
+			byte *b = (byte *)&(*e);
+			byte bucket = b[i >> 1];
+			if(i & 0x01) {
+				bucket >>= 4;
+			}
+			buckets[bucket & 0x0F].push_back(*e);
+		}
+		I first = beg;
+		for(uint j = 0; j != 16; j++) {
+			for(const ElemType &e : buckets[j]) {
+				*first = e;
+				++first;
+			}
+			buckets[j].clear();
+		}
+	}
+}
 
 
 } //core
