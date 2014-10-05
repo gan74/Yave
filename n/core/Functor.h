0a15b3a1f6a78cb3f28b01afb75ead591448f0e6
gan74 <gregoire.angerand@epfl.ch>
2014-10-05 20:06:45 +0200

    Added tuple compatibility and full currying to Functors

--- n/core/Functor.h
+++ n/core/Functor.h
@@ -18,9 +18,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define N_CORE_FUNCTOR_H
 
 #include <n/Types.h>
+#include <tuple>
 #include "SmartPtr.h"
 
 namespace n {
+
+namespace internal {
+
+template<typename R, uint N>
+struct TupleProxy {
+	template<typename T, typename... TArgs, typename... Args>
+	static R apply(T *t, const std::tuple<TArgs...> &tuple, Args... args) {
+		return TupleProxy<R, N - 1>::apply(t, tuple, std::get<N - 1>(tuple), args...);
+	}
+};
+template<typename R>
+struct TupleProxy<R, 0> {
+	template<typename T, typename... TArgs, typename... Args>
+	static R apply(T *t, const std::tuple<TArgs...> &, Args... args) {
+		return (*t)(args...);
+	}
+};
+
+}
+
+
 namespace core {
 
 template<typename R, typename... Args>
@@ -29,6 +51,11 @@ class Functor
 	class FuncBase
 	{
 		public:
+			FuncBase() {
+			}
+
+			FuncBase(const FuncBase &) = delete;
+
 			virtual ~FuncBase() {
 			}
 
@@ -42,12 +69,33 @@ class Functor
 			Func(const T &t) : f(t) {
 			}
 
-			R apply(Args... args) override {
+			Func(const Func<T> &) = delete;
+
+			virtual R apply(Args... args) override {
 				return f(args...);
 			}
 
 		private:
 			T f;
+	};
+
+	class Curry
+	{
+		public:
+			Curry(const Functor<R, Args...> &f, Args... ar) : func(f), args(ar...) {
+			}
+
+			Curry(const Functor<R, Args...> &f, std::tuple<Args...> ar) : func(f), args(ar) {
+			}
+
+			R operator()() {
+				return func(args);
+			}
+
+		private:
+			Functor<R, Args...> func;
+			std::tuple<Args...> args;
+
 	};
 
 	public:
@@ -57,6 +105,18 @@ class Functor
 
 		R operator()(Args... args) {
 			return func->apply(args...);
+		}
+
+		R operator()(const std::tuple<Args...> &args) {
+			return n::internal::TupleProxy<R, sizeof...(Args)>::apply(this, args);
+		}
+
+		Functor<R> curry(Args... args) const {
+			return Functor<R>(Curry(*this, args...));
+		}
+
+		Functor<R> curry(std::tuple<Args...> args) const {
+			return Functor<R>(Curry(*this, args));
 		}
 
 	private:
