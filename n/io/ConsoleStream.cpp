3fbf7c81a4c250a1e6c3feb2782822fe5e45b68c
gan74 <gregoire.angerand@epfl.ch>
2014-10-18 23:02:43 +0200

    Added ConsoleStream

---
+++ n/io/ConsoleStream.cpp
@@ -0,0 +1,60 @@
+/*******************************
+Copyright (C) 2013-2014 gr�goire ANGERAND
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**********************************/
+
+#include "ConsoleStream.h"
+#include <iostream>
+
+namespace n {
+namespace io {
+
+ConsoleInputStream::ConsoleInputStream() : TextInputStream(this) {
+}
+
+bool ConsoleInputStream::atEnd() const {
+	return true;
+}
+
+bool ConsoleInputStream::canRead() const {
+	return true;
+}
+
+uint ConsoleInputStream::readBytes(char *b, uint l) {
+	return std::cin.readsome(b, l);
+}
+
+ConsoleOutputStream::ConsoleOutputStream(Channel ch) : TextOutputStream(this), channel(ch) {
+}
+
+bool ConsoleOutputStream::canWrite() const {
+	return true;
+}
+
+uint ConsoleOutputStream::writeBytes(const char *b, uint len) {
+	(channel == ConsoleOutputStream::Out ? std::cout : std::cerr).write(b, len);
+	return len;
+}
+
+void ConsoleOutputStream::flush() {
+	(channel == ConsoleOutputStream::Out ? std::cout : std::cerr).flush();
+}
+
+
+ConsoleInputStream in = ConsoleInputStream();
+ConsoleOutputStream out = ConsoleOutputStream(ConsoleOutputStream::Out);
+ConsoleOutputStream err = ConsoleOutputStream(ConsoleOutputStream::Error);
+
+}
+}
