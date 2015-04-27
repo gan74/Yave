40e75debbfdb32e52b3f3920df2077408f82e342
gan_ <gregoire.angerand@epfl.ch>
2015-04-27 17:33:16 +0200

    Added OGL verion check (for 4.3)

--- n/graphics/GLContext.cpp
+++ n/graphics/GLContext.cpp
@@ -99,6 +99,14 @@ GLContext::GLContext() : shader(0), program(ShaderProgram::getNullProgram()), fr
 	std::cout<<" (32 bits)";
 	#endif
 	std::cout<<std::endl;
+	
+	int major = 0;
+	int minor = 0;
+	gl::glGetIntegerv(GL_MAJOR_VERSION, &major);
+	gl::glGetIntegerv(GL_MINOR_VERSION, &minor);
+	if(major > 4 || (major == 4 && minor > 3)) {
+		fatal("This needs OpenGL 4.3 or newer to run");
+	}
 
 
 	//gl::glEnable(GL_TEXTURE_2D);
