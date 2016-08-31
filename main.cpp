eb9e3eea7cbe3f99b2ad1a43ad30d9dbdb9219e0
gan74 <gregoire.angerand@epfl.ch>
2016-08-31 14:19:58 +0200

    CMAKE

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
 
-#include <iostream>
-#include <vector>
-
-#include <y/utils.h>
-#include <y/core/Range.h>
-#include <y/test/test.h>
-#include <y/core/Vector.h>
-#include <y/core/Ptr.h>
-
-using namespace y;
-using namespace core;
 
 int main(int, char **) {
 	return 0;
