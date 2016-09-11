3c7da0147e1f1ea7a1ca1d23ea2d086778b08ded
gan74 <gregoire.angerand@epfl.ch>
2016-09-11 22:08:41 +0200

    Removed old work in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,4 @@
-#include <iostream>
-#include <y/io/File.h>
-#include <y/io/BuffReader.h>
-#include <y/io/Decoder.h>
-#include <y/utils.h>
-
-using namespace y;
-using namespace io;
-using namespace core;
-
-using Vertex = std::array<float, 8>;
-using IndexedTriangle = std::array<u32, 3>;
-
-struct MeshData {
-	core::Vector<Vertex> vertices;
-	core::Vector<IndexedTriangle> triangles;
-};
-
-MeshData test(ReaderRef reader) {
-	auto decoder = Decoder(BuffReader(reader));
-
-	u32 magic = 0;
-	u64 version = 0;
-	MeshData mesh;
-
-	decoder.decode<io::Byteorder::BigEndian>(magic);
-	decoder.decode<io::Byteorder::BigEndian>(version);
-
-	if(magic != 0x79617665 || version != 1) {
-		return mesh;
-	}
-
-	decoder.decode<io::Byteorder::BigEndian>(mesh.vertices);
-	decoder.decode<io::Byteorder::BigEndian>(mesh.triangles);
-
-	return mesh;
-}
-
-
 
 int main(int, char **) {
-	auto file = File::open("../../yave/tools/mesh/chalet.ym");
-
-	auto mesh = test(file);
-	std::cout << mesh.triangles.size() << " triangles loaded " << std::endl;
 	return 0;
 }
