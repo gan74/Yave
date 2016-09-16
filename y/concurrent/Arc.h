343b03e7b393d4d6de8d05fa751ccb6ae11c7086
gan74 <gregoire.angerand@epfl.ch>
2016-09-16 17:47:07 +0200

    Arc.

---
+++ y/concurrent/Arc.h
@@ -0,0 +1,42 @@
+/*******************************
+Copyright (C) 2013-2016 gregoire ANGERAND
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**********************************/
+#ifndef Y_CONCURRENT_ARC_H
+#define Y_CONCURRENT_ARC_H
+
+#include <y/core/Ptr.h>
+#include <atomic>
+
+namespace y {
+namespace concurrent {
+
+template<typename T>
+using Arc = core::Rc<T, std::atomic<u32>>;
+
+template<typename T>
+inline auto arc(T&& t) {
+	return Arc<typename std::remove_reference<T>::type>(std::move(t));
+}
+
+}
+
+namespace core {
+using concurrent::Arc;
+using concurrent::arc;
+}
+
+}
+
+#endif // Y_CONCURRENT_ARC_H
