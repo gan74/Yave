13e93f22382cb8eddf43022d318cd62432d077ea
Grégoire Angerand <gregoire.angerand@epfl.ch>
2016-10-08 15:35:33 +0200

    Fixed licence dates.

--- YaveApp.h
+++ YaveApp.h
@@ -1,5 +1,5 @@
 /*******************************
-Copyright (C) 2013-2015 gregoire ANGERAND
+Copyright (C) 2013-2016 gregoire ANGERAND
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
