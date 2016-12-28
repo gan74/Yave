9ec04e1c4fa4acbffe8f3fae442caf56aedb5ff6
Grégoire Angerand <gregoire.angerand@epfl.ch>
2016-12-28 16:31:49 +0100

    Updated shader building script to support computes.

--- shaders/build_shaders.bat
+++ shaders/build_shaders.bat
@@ -1,3 +1,4 @@
 for %%f in (*.frag) do glslangvalidator -V %%f -o ../build/%%f.spv
 for %%f in (*.vert) do glslangvalidator -V %%f -o ../build/%%f.spv
 for %%f in (*.geom) do glslangvalidator -V %%f -o ../build/%%f.spv
+for %%f in (*.comp) do glslangvalidator -V %%f -o ../build/%%f.spv
