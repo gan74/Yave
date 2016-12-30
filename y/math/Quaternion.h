7bd260e41e06d644c41dd78ec04cf54c9faeaf7b
Grégoire Angerand <gregoire.angerand@epfl.ch>
2016-12-30 23:20:11 +0100

    Fixed licence text.

--- y/math/Quaternion.h
+++ y/math/Quaternion.h
@@ -1,8 +1,6 @@
 /*******************************
 Copyright (c) 2016-2017 Grégoire Angerand
 
-This code is licensed under the MIT License (MIT).
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
