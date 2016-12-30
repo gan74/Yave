96adafb189558f75af92a8103f3bb0a7010d1b17
Grégoire Angerand <gregoire.angerand@epfl.ch>
2016-12-30 19:14:31 +0100

    Changed licence to MIT.

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Light core library for [Yave](https://github.com/gan74/Yave).
 ## Building
 You need:
  * Cmake 3.6
- * gcc 5
+ * gcc 6
 
 On Windows [enabling the flto plugin](http://stackoverflow.com/questions/32221221/mingw-x64-windows-plugin-needed-to-handle-lto-object) is needed for release builds.
 
 ### Licence:
-Currently it is GPL, this will change.
+MIT
--- y/core/String.cpp
+++ y/core/String.cpp
@@ -1,17 +1,25 @@
 /*******************************
-Copyright (C) 2013-2016 gregoire ANGERAND
+Copyright (c) 2016-2017 Grégoire Angerand
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+This code is licensed under the MIT License (MIT).
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-GNU General Public License for more details.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 **********************************/
 
 #include "String.h"
@@ -405,7 +413,7 @@ y_test_func("String find") {
 
 
 y_test_func("String unicode") {
-	auto s = str(u8"фa€\u0444");
+	auto s = str(u8"Ñ„aâ‚¬\u0444");
 	y_test_assert(s.size() == 8);
 
 	auto utf = s.to_unicode();
