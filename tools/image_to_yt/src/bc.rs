63052de8642eb948b41dea7886b76227a3503478
Grégoire Angerand <gregoire.angerand@epfl.ch>
2017-01-19 18:51:44 +0100

    Actual Bc1 (bad, but works)

--- tools/image_to_yt/src/bc.rs
+++ tools/image_to_yt/src/bc.rs
@@ -1,7 +1,38 @@
 
+use std::cmp;
 
 fn is_blk(size: (u32, u32)) -> bool {
 	return size.0 % 4 == 0 && size.1 % 4 == 0;
+}
+
+fn bc1_encode(pix: [u8; 4]) -> u16 {
+	((pix[0] as u16 >> 3) << 11) |
+	((pix[1] as u16 >> 2) << 5) |
+	(pix[2] as u16 >> 3)
+}
+
+fn bc1_interp(min: [u8; 4], max: [u8; 4], index: usize) -> [u8; 4] {
+	let coefs = match index {
+		0x0 => (4, 0),
+		0x1 => (3 ,1),
+		0x2 => (1, 3),
+		_ => (0, 4)
+	};
+	let mut pix = [0u8; 4];
+	for i in 0..4 {
+		pix[i] = ((coefs.0 * min[i] as u16 + 
+				   coefs.0 * max[i] as u16) / 4) as u8
+	}
+	pix
+}
+
+fn dist(a: &[u8; 4], b: &[u8; 4]) -> u32 {
+	let mut sum = 0u32;
+	for i in 0..4 {
+		let x = if a[i] < b[i] { b[i] - a[i] } else { a[i] - b[i] } as u32;
+		sum += x * x;
+	}
+	sum
 }
 
 pub fn bc1(image: &Vec<u8>, size: (u32, u32)) -> Option<Vec<u8>> {
@@ -33,26 +64,62 @@ pub fn bc1(image: &Vec<u8>, size: (u32, u32)) -> Option<Vec<u8>> {
 		out.push(0x22 as u8);
     }*/
 
-   	for x in 0..(size.0 / 4) {
-   		for y in 0..(size.1 / 4) {
-   			let index = (y * 4 + x * 4 * size.0) as usize * 4 ;
+    let size = (size.0 as usize, size.1 as usize);
 
-			let rgb = (image[index] as u16, image[index + 1] as u16, image[index + 2] as u16);
+    let block_size = 4;
 
-			let pixel: u16 = ((rgb.0 >> 3) << 11) |
-							 ((rgb.1 >> 2) << 5) |
-							 (rgb.2 >> 3);
+    let blk_count = (size.0 / block_size, size.1 / block_size);
 
-			out.push((pixel & 0xFF) as u8);
-			out.push((pixel >> 8) as u8);
+   	for x in 0..blk_count.0 {
+   		for y in 0..blk_count.1 {
 
-			out.push((pixel & 0xFF) as u8);
-			out.push((pixel >> 8) as u8);
+   			let mut pixels = [[0u8; 4]; 16]; // No compile time constants
+   			for px in 0..block_size {
+   				for py in 0..block_size {
+   					let x = x * block_size + px;
+   					let y = y * block_size + py;
 
-			out.push(0x22 as u8);
-			out.push(0x22 as u8);
-			out.push(0x22 as u8);
-			out.push(0x22 as u8);
+   					let pi = (x * size.0 + y) * 4; // in bytes
+   					let pix = [image[pi], image[pi + 1], image[pi + 2], image[pi + 3]];
+   					pixels[px + py * 4] = pix;
+   				}
+   			}
+
+   			let mut min = [255u8; 4];
+   			let mut max = [0u8; 4];
+   			for p in pixels.into_iter() {
+   				for i in 0..4 {
+   					min[i] = cmp::min(min[i], p[i]);
+   					max[i] = cmp::max(max[i], p[i]);
+   				}
+   			}
+
+   			let mut interps = [[0u8; 4]; 4];
+   			for i in 0..4 {
+   				interps[i] = bc1_interp(min, max, i);
+   			}
+   			// if we want alpha swap min/max (or encode min first)
+
+			let min = bc1_encode(min);
+			let max = bc1_encode(max);
+
+			out.push((max & 0xFF) as u8);
+			out.push((max >> 8) as u8);
+
+			out.push((min & 0xFF) as u8);
+			out.push((min >> 8) as u8);
+
+			let mut mask = 0u32;
+
+			for pix in pixels.into_iter() {
+				let best = (0..4).into_iter().min_by_key(|x| dist(&interps[*x], pix)).unwrap() as u32;
+				mask = (mask >> 2) | ((best & 0x03) << 30);
+			}
+
+			for _ in 0..4 {
+				out.push((mask & 0xFF) as u8);
+				mask >>= 8;
+			}
 		}
 	}
 
