d5aaf45b81616ec84c57b63ad9f6d890719013c0
Grégoire Angerand <gregoire.angerand@epfl.ch>
2017-01-26 17:25:06 +0100

    More texture encoder work

--- tools/image_to_yt/src/bc.rs
+++ tools/image_to_yt/src/bc.rs
@@ -1,17 +1,20 @@
 
 use std::cmp;
 
+type Endpoint = [u16; 4];
+type Rgba = [u8; 4];
+
 fn is_blk(size: (usize, usize)) -> bool {
 	return size.0 % 4 == 0 && size.1 % 4 == 0;
 }
 
-fn bc1_encode_endpoint(pix: &[i16; 4]) -> i16 {
+fn bc1_encode_endpoint(pix: &Endpoint) -> u16 {
 	((pix[0] >> 3) << 11) |
 	((pix[1] >> 2) << 5) |
 	(pix[2] >> 3)
 }
 
-fn bc1_interp(min: &[i16; 4], max: &[i16; 4], index: usize) -> [u8; 4] {
+fn bc1_build_entry(min: &Endpoint, max: &Endpoint, index: usize) -> Rgba {
 	let coefs = match index {
 		0x0 => (3, 0),
 		0x1 => (0, 3),
@@ -24,21 +27,21 @@ fn bc1_interp(min: &[i16; 4], max: &[i16; 4], index: usize) -> [u8; 4] {
 	 ((coefs.1 * min[3] + coefs.0 * max[3]) / 3) as u8]
 }
 
-fn bc1_table(min: &[i16; 4], max: &[i16; 4]) -> [[u8; 4]; 4] {
+fn bc1_build_table(min: &Endpoint, max: &Endpoint) -> [Rgba; 4] {
 	let mut interps = [[0u8; 4]; 4];
 	for i in 0..4 {
-		interps[i] = bc1_interp(&min, &max, i);
+		interps[i] = bc1_build_entry(&min, &max, i);
 	}
 	interps
 }
 
-fn bc1_minmax(pixels: &[[u8; 4]; 16]) -> ([i16; 4], [i16; 4]) {
-	let mut min = [255i16; 4];
-	let mut max = [0i16; 4];
+fn bc1_minmax(pixels: &[Rgba; 16]) -> (Endpoint, Endpoint) {
+	let mut min = [256u16; 4];
+	let mut max = [0u16; 4];
 	for p in pixels.into_iter() {
 		for i in 0..4 {
-			min[i] = cmp::min(min[i], p[i] as i16);
-			max[i] = cmp::max(max[i], p[i] as i16);
+			min[i] = cmp::min(min[i], p[i] as u16);
+			max[i] = cmp::max(max[i], p[i] as u16);
 		}
 	}
 	(min, max)
@@ -48,14 +51,14 @@ fn bc1_dist_one(a: i16, b: i16) -> u32 {
 	(a - b).abs() as u32
 }
 
-fn bc1_dist(a: &[u8; 4], b: &[u8; 4]) -> u32 {
+fn bc1_dist(a: &Rgba, b: &Rgba) -> u32 {
 	bc1_dist_one(a[0] as i16, b[0] as i16) + 
 	bc1_dist_one(a[1] as i16, b[1] as i16) + 
 	bc1_dist_one(a[2] as i16, b[2] as i16) + 
 	bc1_dist_one(a[3] as i16, b[3] as i16)
 }
 
-fn bc1_encode(table: &[[u8; 4]; 4], pixels: &[[u8; 4]; 16]) -> u32 {
+fn bc1_encode_pixel(table: &[Rgba; 4], pixels: &[Rgba; 16]) -> u32 {
 	let mut mask = 0u32;
 	for pix in pixels.into_iter().rev() {
 		let best = (0..4).into_iter().min_by_key(|x| bc1_dist(&table[*x], pix)).unwrap() as u32;
@@ -64,8 +67,7 @@ fn bc1_encode(table: &[[u8; 4]; 4], pixels: &[[u8; 4]; 16]) -> u32 {
 	mask
 }
 
-
-fn bc1_total_dist(table: &[[u8; 4]; 4], pixels: &[[u8; 4]; 16]) -> u32 {
+/*fn bc1_total_dist(table: &[Rgba; 4], pixels: &[Rgba; 16]) -> u32 {
 	let mut sum = 0u32;
 	for pix in pixels.into_iter().rev() {
 		sum += (0..4).into_iter().map(|x| bc1_dist(&table[x], pix)).min().unwrap();
@@ -73,13 +75,13 @@ fn bc1_total_dist(table: &[[u8; 4]; 4], pixels: &[[u8; 4]; 16]) -> u32 {
 	sum
 }
 
-fn iter_endpoint(mut to_iter: [i16; 4], other: &[i16; 4], pixels: &[[u8; 4]; 16], offsets: &[i16]) -> [i16; 4] {
-	let (mut best, mut index, mut offset) = (bc1_total_dist(&bc1_table(&to_iter, other), pixels), 0, 0);
+fn iter_endpoint(mut to_iter: Endpoint, other: &Endpoint, pixels: &[Rgba; 16], offsets: &[i16]) -> [u16; 4] {
+	let (mut best, mut index, mut offset) = (bc1_total_dist(&bc1_build_table(&to_iter, other), pixels), 0, 0);
 	for i in 0..3 {
 		for o in offsets.into_iter() {
 			let mut x = to_iter;
 			x[i] += *o;
-			let table = bc1_table(&x, &other);
+			let table = bc1_build_table(&x, &other);
 			let dist = bc1_total_dist(&table, pixels);
 			if dist < best {
 				best = dist;
@@ -90,17 +92,32 @@ fn iter_endpoint(mut to_iter: [i16; 4], other: &[i16; 4], pixels: &[[u8; 4]; 16]
 	}
 	to_iter[index] += offset;
 	to_iter
+}*/
+
+fn bc1_encode_block(pixels: &[Rgba; 16]) -> u64 {
+	let (min, max) = bc1_minmax(&pixels);
+
+	/*for _ in 0..quality {
+		min = iter_endpoint(min, &max, &pixels, &[2, 4, 8]);
+		max = iter_endpoint(max, &min, &pixels, &[-2, -4, -8]);
+	}*/
+
+	let table = bc1_build_table(&min, &max);
+
+	let min = bc1_encode_endpoint(&min) as u64;
+	let max = bc1_encode_endpoint(&max) as u64;
+
+	let encoded = bc1_encode_pixel(&table, &pixels) as u64;
+
+	(encoded << 32) | (min << 16) | max
 }
 
-
-pub fn bc1(image: &Vec<u8>, size: (usize, usize), quality: u8) -> Result<Vec<u8>, ()> {
+pub fn bc1(image: &Vec<u8>, size: (usize, usize), _: u8) -> Result<Vec<u8>, ()> {
 	if !is_blk(size) {
 		return Err(());
 	}
 
     let mut out = Vec::with_capacity(image.len() / 8);
-
-   	//let mut total_distance = 0u64;
 
     let block_size = 4;
     let blk_count = (size.0 / block_size, size.1 / block_size);
@@ -121,34 +138,13 @@ pub fn bc1(image: &Vec<u8>, size: (usize, usize), quality: u8) -> Result<Vec<u8>
    				}
    			}
 
-   			let (mut min, mut max) = bc1_minmax(&pixels);
+   			let mut encoded = bc1_encode_block(&pixels);
 
-   			for _ in 0..quality {
-   				min = iter_endpoint(min, &max, &pixels, &[2, 4, 8]);
-   				max = iter_endpoint(max, &min, &pixels, &[-2, -4, -8]);
+   			for _ in 0..8 {
+   				out.push((encoded & 0xFF) as u8);
+   				encoded >>= 8;
    			}
-
-   			let table = bc1_table(&min, &max);
-
-   			//total_distance += bc1_total_dist(&table, &pixels) as u64;
-
-   			// if we want alpha swap min/max (or encode min first)
-			let min = bc1_encode_endpoint(&min);
-			let max = bc1_encode_endpoint(&max);
-
-			out.push((max & 0xFF) as u8);
-			out.push((max >> 8) as u8);
-
-			out.push((min & 0xFF) as u8);
-			out.push((min >> 8) as u8);
-
-			let mut encoded = bc1_encode(&table, &pixels);
-			for _ in 0..4 {
-				out.push((encoded & 0xFF) as u8);
-				encoded >>= 8;
-			}
 		}
 	}
-	//println!("total = {}\nper-pixel = {}", total_distance, total_distance as f64/(size.0 * size.1) as f64);
 	Ok(out)
 }
