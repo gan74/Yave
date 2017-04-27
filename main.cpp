fc977d0c21010fc3885983e50bc0e9693aa0d774
Grégoire Angerand <gregoire.angerand@epfl.ch>
2017-04-27 19:01:10 +0200

    camera.

--- main.cpp
+++ main.cpp
@@ -1,68 +1,33 @@
-#include <y/concurrent/WorkGroup.h>
-#include <y/core/Chrono.h>
-#include <y/concurrent/SpinLock.h>
 
+#include <y/core/Chrono.h>
+#include <y/core/Lazy.h>
+#include <y/math/math.h>
 
 #include <unordered_map>
 #include <iostream>
+#include <iomanip>
 
 using namespace y;
-using namespace y::concurrent;
+using namespace y::core;
 
-static std::unordered_map<std::thread::id, usize> result;
-static SpinLock mutex;
-
-void done() {
-	std::lock_guard<decltype(mutex)> _(mutex);
-	auto x = result.insert(std::make_pair(std::this_thread::get_id(), 1));
-	if(!x.second) {
-		(*x.first).second++;
-	}
+int create() {
+	std::cout << "create()" << std::endl;
+	return 7;
 }
-
-void test() {
-	core::Vector<int> v;
-	for(int i = 0; i != 100000; ++i) {
-		v << i;
-	}
-
-	WorkGroup(10).schedule_range(v.begin(), v.end(),
-		[](int) {
-			done();
-		});
-}
-
 
 int main() {
 #ifdef Y_BUILD_TESTS
 	return 0;
 #endif
 
-	core::Chrono ch;
-	test();
-	std::cout << "DONE in " << ch.elapsed().to_secs() << "s" << std::endl;
+	Lazy<int> i(create);
+
+	std::cout << "lazy = " << i() << std::endl;
+
+	std::cout << 14 << std::endl;
 
 
-	usize min = 10000;
-	usize max = 0;
-	for(auto e : result)  {
-		usize s = e.second;
-		min = std::min(min, s);
-		max = std::max(max, s);
-	}
-
-	std::cout << "partition size in [" << min << ", " << max << "]" << std::endl;
-
-	usize total = 0;
-	usize id = 0;
-	for(auto e : result)  {
-		std::cout << "thread #" << id++ << " = " << e.second << std::endl;
-		total += e.second;
-	}
-
-	std::cout << "total = " << total << std::endl;
-
-
+	std::cout << std::setprecision(25) << math::pi<float> << '\n' << math::pi<double> << std::endl;
 	return 0;
 }
 
