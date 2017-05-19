90f50de14a9727737752d59a2ed170a5c024c0af
Grégoire Angerand <gregoire.angerand@epfl.ch>
2017-05-19 20:47:13 +0200

    Added missing licenses.

--- main.cpp
+++ main.cpp
@@ -29,67 +29,36 @@ int main() {
 	return 0;
 #endif
 
-	concurrent::init_thread_pool();
-
 	core::Vector<int> v(5000u, 0);
 	int index = 0;
 	for(auto& i : v) {
 		i = index++;
 	}
 
-	/*{
-		Chrono timer;
-		usize loops = 0;
-		while(timer.elapsed().to_secs() < 10) {
-			concurrent::parallel_for_each(v.begin(), v.end(), [&](auto&& i) {
-				slow(i);
-			});
-			loops++;
-		}
-		std::cout << "parallel_for_each: " << loops << std::endl;
-	}
-
 	{
-		Chrono timer;
-		usize loops = 0;
-		while(timer.elapsed().to_secs() < 10) {
-			volatile usize sum = 0;
-			concurrent::parallel_block_for(0, 100, [&](auto&&) {
-				sum += 1;
-			});
-			loops++;
-		}
-		std::cout << "parallel_block_for spam: " << loops << std::endl;;
-	}
-
-	{
-		Chrono timer;
-		usize loops = 0;
-		while(timer.elapsed().to_secs() < 10) {
-			auto col = concurrent::parallel_collect(v.begin(), v.end(), [&](auto&& range) {
-				core::Vector<int> v;
-				for(auto i : range) {
-					v.push_back(i);
-				}
-				return v;
-			});
-			std::sort(col.begin(), col.end());
-			if(v != col) {
-				fatal("invalid result");
-			}
-			loops++;
-		}
-		std::cout << "parallel_collect: " << loops << std::endl;;
-	}*/
-
-	auto thread = new std::thread([]() {
-		concurrent::parallel_for(0, 1, [](auto&&) {
-			std::cout << "started..." << std::endl;
-			std::this_thread::sleep_for(std::chrono::seconds(10));
-			std::cout << "done!" << std::endl;
+		DebugTimer timer("parallel_for_each(1)");
+		concurrent::parallel_for_each(v.begin(), v.end(), [&](auto&& i) {
+			slow(i);
 		});
-	});
-	thread->detach();
+	}
+
+	{
+		DebugTimer timer("std::for_each");
+		std::for_each(v.begin(), v.end(), [&](auto&& i) {
+			slow(i);
+		});
+	}
+
+	concurrent::init_thread_pool();
+
+	{
+		DebugTimer timer("parallel_for_each(n)");
+		concurrent::parallel_for_each(v.begin(), v.end(), [&](auto&& i) {
+			slow(i);
+		});
+	}
+
+
 
 	return 0;
 }
--- tests/math.cpp
+++ tests/math.cpp
@@ -1,5 +1,5 @@
 /*******************************
-Copyright (c) 2016-2017 Grégoire Angerand
+Copyright (c) 2016-2017 Gr�goire Angerand
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,13 +19,41 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 **********************************/
-#include "Writer.h"
 
-namespace y {
-namespace io {
+#include <y/utils.h>
+#include <y/test/test.h>
 
-Writer::~Writer() {
+
+#include <y/math/math.h>
+
+
+using namespace y;
+using namespace y::math;
+
+Vec3 X(1.0f, 0.0f, 0.0f);
+Vec3 Y(0.0f, 1.0f, 0.0f);
+Vec3 Z(0.0f, 0.0f, 1.0f);
+
+template<usize N>
+static bool eq(const Vec<N>& a, const Vec<N>& b) {
+	return (a - b).length2() < 0.001f;
 }
 
+template<usize N>
+static bool not_n(const Vec<N>& a) {
+	return a.length2() > 0.001f;
 }
+
+template<usize N>
+static bool eq_not_n(const Vec<N>& a, const Vec<N>& b) {
+	return not_n(a) && eq(a, b);
 }
+
+
+y_test_func("math basic rotation") {
+	auto rot = rotation(pi<float>, Z);
+	y_test_assert(eq_not_n(rot * Vec4(X, 0.0f), Vec4(-X, 0.0f)));
+}
+
+
+
