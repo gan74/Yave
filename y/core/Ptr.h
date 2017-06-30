f4046f37f4ec54771f855f7b80d69184776eb944
Grégoire Angerand <gregoire.angerand@epfl.ch>
2017-06-30 21:04:20 +0200

    Added ptr::value_type

--- y/core/Ptr.h
+++ y/core/Ptr.h
@@ -32,6 +32,7 @@ namespace detail {
 template<typename T>
 class Ptr : NonCopyable {
 	public:
+		using value_type = std::remove_reference_t<T>;
 		using pointer = std::remove_extent_t<T>*;
 		using const_pointer = const std::remove_extent_t<T>*;
 
