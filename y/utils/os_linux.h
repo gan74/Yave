55736150eac69a35cc21525663b5912e8498f53b
gan74 <gregoire.angerand@epfl.ch>
2017-09-25 18:42:22 +0200

    Add linux defines & includes

--- y/utils/os_linux.h
+++ y/utils/os_linux.h
@@ -1,5 +1,5 @@
 /*******************************
-Copyright (c) 2016-2017 Grégoire Angerand
+Copyright (c) 2016-2017 Gr�goire Angerand
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,34 +19,16 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 **********************************/
-#ifndef Y_DEFINES_H
-#define Y_DEFINES_H
+#ifndef Y_UTILS_OS_LINUX_H
+#define Y_UTILS_OS_LINUX_H
 
-namespace y {
+#include <y/defines.h>
+#include "types.h"
 
-struct Nothing;
-Nothing fatal(const char* msg, const char* file = nullptr, int line = 0);
+#ifdef Y_OS_LINUX
 
-}
+#include <sys/types.h>
+#include <unistd.h>
 
-
-/*
-// For some reasons this destroys sol2 (freeze at runtime)
-#ifndef __PRETTY_FUNCTION__
-#define __PRETTY_FUNCTION__ __func__
-#endif
-*/
-
-/****************** OS DEFINES BELOW ******************/
-
-#if defined(WIN32) || defined(__WIN32) || defined(__WIN32__) || defined(_WINDOWS)
-#define Y_OS_WIN
-#define WIN32_LEAN_AND_MEAN
-#endif
-
-#if defined(__linux__) || defined(__gnu_linux__)
-#define Y_OS_LINUX
-#endif
-
-
-#endif // Y_DEFINES_H
+#endif // Y_OS_LINUX
+#endif // Y_UTILS_OS_LINUX_H
