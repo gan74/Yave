bfe04f502eb6a573c484797c0e2e85b820a6f045
gan74 <gregoire.angerand@epfl.ch>
2018-03-02 15:01:52 +0100

    Copyright update.

--- y/core/Functor.h
+++ y/core/Functor.h
@@ -1,5 +1,5 @@
 /*******************************
-Copyright (c) 2016-2017 Grégoire Angerand
+Copyright (c) 2016-2018 Grégoire Angerand
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -110,7 +110,7 @@ class Functor<Container, Ret(Args...)> {
 			return _function != other._function;
 		}
 
-		Ret operator()(Args&&... args) {
+		Ret operator()(Args&&... args) const {
 			return _function->apply(std::forward<Args>(args)...);
 		}
 
