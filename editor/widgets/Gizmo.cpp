260ba65553720d392efb3c91bad30d10212391eb
gan74 <gregoire.angerand@gmail.com>
2019-12-07 13:29:21 +0100

    Made gizmo always face camera

--- editor/widgets/Gizmo.cpp
+++ editor/widgets/Gizmo.cpp
@@ -179,6 +179,12 @@ void Gizmo::draw() {
 		}
 	}
 
+	for(math::Vec3& a : basis) {
+		if(cam_fwd.dot(a) > 0.0f) {
+			a = -a;
+		}
+	}
+
 
 	if(_mode == Translate) {
 		struct Axis {
@@ -287,8 +293,6 @@ void Gizmo::draw() {
 			}
 		}
 	} else if(_mode == Rotate) {
-		Y_TODO(Rotation doesnt work when far from the origin)
-
 		const usize segment_count = 64;
 		const float seg_ang_size = (1.0f / segment_count) * 2.0f * math::pi<float>;
 		usize rotation_axis = _rotation_axis;
