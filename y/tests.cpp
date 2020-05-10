c75396c77cabb3e699a878103f988c5e94e26b85
gan74 <gregoire.angerand@gmail.com>
2020-05-10 23:38:13 +0200

    Map benches

--- y/tests.cpp
+++ y/tests.cpp
@@ -26,6 +26,44 @@ SOFTWARE.
 
 using namespace y;
 
+#if 0
+#include <y/core/Chrono.h>
+#include <y/core/DenseMap.h>
+#include <y/utils/format.h>
+#include <unordered_map>
+
+template<typename Map>
+int bench(int max_key) {
+	int sum = 0;
+
+	double runs_d = 1000000.0 / max_key;
+	const usize runs = runs_d < 1.0 ? 1 : usize(runs_d);
+
+	for(usize r = 0; r != runs; ++r) {
+		Map map;
+		for(int i = 0; i != max_key; ++i) {
+			map.insert({i, i * 2});
+		}
+
+		usize map_size = max_key;
+		unused(map_size);
+		for(int i = 0; i < max_key; i += 1 + (i / 8)) {
+			const auto it = map.find(i);
+			y_debug_assert(it != map.end());
+			map.erase(it);
+			--map_size;
+			y_debug_assert(map_size == map.size());
+		}
+
+		for(auto&& [k, v] : map) {
+			sum += k;
+			sum += v / 2;
+		}
+	}
+	return sum;
+}
+#endif
+
 int main() {
 	const bool ok = test::run_tests();
 
@@ -34,6 +72,31 @@ int main() {
 	} else {
 		log_msg("Tests failed\n", Log::Error);
 	}
+
+#if 0
+	for(int max_key = 1; max_key < 1024 * 1024; max_key *= 2) {
+		log_msg(fmt("Size: %", max_key), Log::Debug);
+		{
+			core::DebugTimer _("ExternalDenseMap");
+			bench<core::ExternalDenseMap<int, int>>(max_key);
+		}
+
+		{
+			core::DebugTimer _("  StableDenseMap");
+			bench<core::StableDenseMap<int, int>>(max_key);
+		}
+
+		{
+			core::DebugTimer _("        DenseMap");
+			bench<core::DenseMap<int, int>>(max_key);
+		}
+
+		{
+			core::DebugTimer _("   unordered_map");
+			bench<std::unordered_map<int, int>>(max_key);
+		}
+	}
+#endif
 
 	return 0;
 }
