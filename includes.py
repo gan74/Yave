f0aef57e6a8bbce310fb46b8c393923118a37443
Grégoire Angerand <gregoire.angerand@ubisoft.com>
2020-08-03 11:22:57 +0200

    Cleaned up scripts

--- includes.py
+++ includes.py
@@ -4,119 +4,119 @@ import os
 import subprocess
 
 
-def try_compile():
-	p = subprocess.call(["mingw32-make", "-j6"], cwd = "build/debug")
-	return p == 0
-
 def extension(file):
-	return file.split(".")[-1]
-	
-	
-def try_cpp(filename, line):
-	if not filename.endswith(".h"):
-		return False
-	fullname = filename[:-2] + ".cpp"
-	if not os.path.isfile(fullname):
-		return False
-	with open(fullname, "r") as f:
-		content = f.read()
-	with open(fullname, "w") as f:
-		f.write(line + "\n" + content)
-	if try_compile():
-		return True
-	with open(fullname, "w") as f:
-		f.write(content)
-	return False
-	
+    return file.split(".")[-1]
+    
+
+def clean_name(name):
+    return name.replace("\\\\", "/").replace("\\", "/")
+
+
 def process_file(fullname):
-	content = ""
-	with open(fullname, "r") as f:
-		content = f.read()
-	
-	run = True
-	while run:
-		run = False
-		for line in content.split("\n"):
-			if line.startswith("#include "):
-				sep = ["\"", "\""] 
-				if "<" in line:
-					sep = ["<", ">"]
-				
-				start = line.find(sep[0]) + 1
-				end = line.find(sep[1], start)
-				name = line[start:end]
-				if name.endswith(".h"):
-					name = line[start:end-2]
-				slash = name.rfind("/")
-				if slash >= 0:
-					name = name[slash+1:]
-					
-				if name not in classes:
-					print(name, "is unknown")
-					continue
-				
-				new_line = "namespace " + classes[name] + " { class " + name + "; }"
-				print(line, "=>", new_line)
-				new_content = content.replace(line, new_line)
-				
-				with open(fullname, "w") as f:
-					f.write(new_content)
-				
-				
-				if try_compile() or try_cpp(fullname, line):
-					print("Success")
-					content = new_content
-					run = True
-					break
-				else:
-					print("Failed")
-					
-	
-	with open(fullname, "w") as f:
-		f.write(content)
-	
-
-	
-	
-	
+    content = ""
+    with open(fullname, "r") as f:
+        content = f.read()
+        
+    parent = ""
+    s = fullname.rfind("/")
+    if s >= 0:
+        parent = fullname[:s] + "/"
+    
+    includes = []
+    for line in content.split("\n"):
+        if line.startswith("#include "):
+            sep = ["\"", "\""] 
+            absolute = False
+            if "<" in line:
+                sep = ["<", ">"]
+                absolute = True
+            
+            start = line.find(sep[0]) + 1
+            end = line.find(sep[1], start)
+            name = line[start:end]
+            if not name.endswith(".h"):
+                continue
+            if not absolute:
+                name = parent + name
+            name = clean_name(name)
+            if name not in includes:
+                includes.append(name)
+    return includes
+    
+    
 
 
-#find classes
-def find_classes(folders):
-	find_class = re.compile(r"class ([A-Za-z]+)")
-	find_struct = re.compile(r"struct ([A-Za-z]+)")
-	for proj in folders:
-		for root, dirs, files in os.walk(proj):
-			for file in files:
-				if extension(file) not in {"h"}:
-					continue
-				fullname = root + "/" + file
-				with open(fullname, "r") as f:
-					content = f.read()
-				cl = find_class.findall(content)
-				st = find_struct.findall(content)
-				for c in cl:
-					classes[c] = proj
-				for s in st:
-					classes[s] = proj
-				
-				
-#remove includes
 def process_includes(folders):
-	for proj in folders:
-		for root, dirs, files in os.walk(proj):
-			for file in files:
-				if extension(file) not in {"h"}:
-					continue
-				fullname = root + "/" + file
-				
-				print("starting", file)
-				process_file(fullname)
-				print(file, "done")
-				
-	
-	
-projects = {"yave", "editor"}
+    includes = {}
+    for proj in folders:
+        for root, dirs, files in os.walk(proj):
+            for file in files:
+                if extension(file) not in {"h", "cpp"}:
+                    continue
+                fullname = clean_name(root + "/" + file)
+                
+                includes[fullname] = process_file(fullname)
+    return includes
+    
+    
+def print_includes(file, includes, tabs, printed):
+    total = 0
+    if file in printed:
+        # print("    " * tabs, "*", file)
+        return total
+        
+    print("\t" * tabs + file)
+    printed.add(file)
+    
+    try:
+        for i in includes[file]:
+            total += print_includes(i, includes, tabs + 1, printed)
+    except:
+        print("\t" * (tabs + 1) + "????")
+        pass
+        
+    return total + 1
+
+def to_sorted(incs):
+    l = [(i, c) for i, c in incs.items()]
+    l.sort(key = lambda p : p[1], reverse = True)
+    return l
+
+
+
+
+
+projects = {"yave"}
 classes = {}
-find_classes(projects)
-process_includes(projects)
+includes = process_includes(projects)
+
+include_count = {}
+for file in includes.keys():
+    printed = set()
+    include_count[file] = print_includes(file, includes, 0, printed)
+    print()
+    print()
+
+
+included = {}
+for f in includes.keys():
+    included[f] = 1
+for f in includes.values():
+    for i in f:
+        try:
+            included[i] += 1
+        except:
+            pass
+
+
+
+
+
+for p in to_sorted(include_count):
+    print(p[0] + " " * (60 - len(p[0])) + "=>", p[1]) 
+    
+    
+print()
+print()
+for p in to_sorted(included):
+    print(p[0] + " " * (60 - len(p[0])) + "included", p[1], "total", p[1] * included[p[0]]) 
