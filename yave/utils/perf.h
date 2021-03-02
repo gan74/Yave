89b94a759ee732936ad7305c2e1a6dc222ff15cc
gan74 <gregoire.angerand@gmail.com>
2021-03-02 02:16:14 +0100

    Small cleanup

--- yave/utils/perf.h
+++ yave/utils/perf.h
@@ -25,19 +25,16 @@ SOFTWARE.
 #include <y/utils.h>
 
 #ifdef Y_DEBUG
-#ifndef YAVE_PERF_LOG_DISABLED
+#if !defined(YAVE_PERF_LOG_DISABLED) && !defined(YAVE_PERF_LOG_ENABLED)
 #define YAVE_PERF_LOG_ENABLED
 #endif
 #endif
 
 
-#ifdef YAVE_PERF_LOG_ENABLED
-#ifndef TRACY_ENABLE
+#if defined(YAVE_PERF_LOG_ENABLED) && !defined(TRACY_ENABLE)
 #error TRACY_ENABLE should be set if YAVE_PERF_LOG_ENABLED is set
 #endif
-#endif
 
-#include <external/tracy/Tracy.hpp>
 #include <external/tracy/TracyC.h>
 
 namespace yave {
@@ -47,31 +44,25 @@ using namespace y;
 
 #ifdef YAVE_PERF_LOG_ENABLED
 
-inline void start_capture(const char* out_filename) {
+static constexpr bool always_capture = true;
 
-}
+void start_capture(const char* out_filename);
+void end_capture();
+bool is_capturing();
 
-inline void end_capture() {
+#define y_profile_capturing() (yave::perf::always_capture || yave::perf::is_capturing())
 
-}
-
-inline bool is_capturing() {
-    return false;
-}
-
-#define y_profile_frame_end() do { TracyCFrameMark } while(false)
+#define y_profile_frame_end() do { ___tracy_emit_frame_mark(nullptr); } while(false)
 
 #define y_profile()                                                                                                                             \
-    static const auto y_create_name_with_prefix(sloc) = ___tracy_source_location_data { nullptr, __FUNCTION__, __FILE__, __LINE__, 0};          \
-    const auto y_create_name_with_prefix(ctx) = ___tracy_emit_zone_begin(&y_create_name_with_prefix(sloc), true);                               \
+    static constexpr auto y_create_name_with_prefix(sloc) = ___tracy_source_location_data { nullptr, __FUNCTION__, __FILE__, __LINE__, 0};      \
+    const auto y_create_name_with_prefix(ctx) = ___tracy_emit_zone_begin(&y_create_name_with_prefix(sloc), y_profile_capturing());              \
     y_defer(___tracy_emit_zone_end(y_create_name_with_prefix(ctx)))
 
 #define y_profile_zone(name)                                                                                                                    \
-    static const auto y_create_name_with_prefix(sloc) = ___tracy_source_location_data { nullptr, __FUNCTION__, __FILE__, __LINE__, 0};          \
+    y_profile();                                                                                                                                \
     const char* y_create_name_with_prefix(zone) = (name);                                                                                       \
-    const auto y_create_name_with_prefix(ctx) = ___tracy_emit_zone_begin(&y_create_name_with_prefix(sloc), true);                               \
     ___tracy_emit_zone_name(y_create_name_with_prefix(ctx), y_create_name_with_prefix(zone), std::strlen(y_create_name_with_prefix(zone)));     \
-    y_defer(___tracy_emit_zone_end(y_create_name_with_prefix(ctx)))
 
 #define y_profile_unique_lock(inner) [&]() {                            \
         std::unique_lock l(inner, std::defer_lock);                     \
