1d6bd9b1ddc7816157e9b0b116cd3a8282ec4e3d
gan74 <gregoire.angerand@gmail.com>
2022-09-26 22:06:36 +0200

    Added missing file

---
+++ yave/ecs/tags.h
@@ -0,0 +1,52 @@
+/*******************************
+Copyright (c) 2016-2022 Gr�goire Angerand
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**********************************/
+#ifndef YAVE_ECS_TAGS_H
+#define YAVE_ECS_TAGS_H
+
+#include <yave/yave.h>
+
+#include <y/core/String.h>
+
+namespace yave {
+namespace ecs {
+namespace tags {
+
+#define DECLARE_TAG(tag)                                \
+static const core::String tag = #tag;                   \
+static const core::String not_##tag = "!"#tag;
+
+
+
+DECLARE_TAG(hidden)
+DECLARE_TAG(selected)
+
+
+
+#undef DECLARE_TAG
+
+}
+}
+}
+
+
+#endif // YAVE_ECS_TAGS_H
+
