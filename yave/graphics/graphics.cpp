7a96f34a6183e2d62b08c23b3dfef56259c8b726
gan74 <gregoire.angerand@gmail.com>
2023-09-01 22:15:57 +0200

    More mutexed

--- yave/graphics/graphics.cpp
+++ yave/graphics/graphics.cpp
@@ -35,9 +35,9 @@ SOFTWARE.
 #include <yave/graphics/device/LifetimeManager.h>
 #include <yave/graphics/device/MeshAllocator.h>
 
+#include <y/concurrent/Mutexed.h>
 #include <y/core/ScratchPad.h>
 
-#include <mutex>
 
 namespace yave {
 namespace device {
@@ -75,10 +75,7 @@ struct {
 struct {
 } extensions;
 
-struct {
-    std::mutex lock;
-    core::Vector<std::pair<std::unique_ptr<ThreadLocalDevice>, ThreadDevicePtr*>> devices;
-} threads;
+concurrent::Mutexed<core::Vector<std::pair<std::unique_ptr<ThreadLocalDevice>, ThreadDevicePtr*>>> thread_devices;
 
 }
 
@@ -226,11 +223,12 @@ void destroy_device() {
     y_always_assert(device::active_pools == 1, "Not all pools have been destroyed");
     {
         y_profile_zone("collecting thread devices");
-        const auto lock = y_profile_unique_lock(device::threads.lock);
-        for(const auto& p : device::threads.devices) {
-            *p.second = nullptr;
-        }
-        device::threads.devices.clear();
+        device::thread_devices.locked([](auto&& devices) {
+            for(const auto& p : devices) {
+                *p.second = nullptr;
+            }
+            devices.clear();
+        });
     }
     y_always_assert(device::active_pools == 0, "Not all pools have been destroyed");
 
@@ -272,15 +270,16 @@ ThreadDevicePtr thread_device() {
     static thread_local y_defer({
         if(auto* device = thread_device) {
             y_debug_assert(device_initialized());
-            const auto lock = y_profile_unique_lock(device::threads.lock);
-            const auto it = std::find_if(device::threads.devices.begin(), device::threads.devices.end(), [&](const auto& p) { return p.first.get() == device; });
+            device::thread_devices.locked([&](auto&& devices) {
+                const auto it = std::find_if(devices.begin(), devices.end(), [&](const auto& p) { return p.first.get() == device; });
 
-            y_always_assert(it != device::threads.devices.end(), "ThreadLocalDevice not found.");
-            y_debug_assert(*it->second == device);
+                y_always_assert(it != devices.end(), "ThreadLocalDevice not found.");
+                y_debug_assert(*it->second == device);
 
-            *it->second = nullptr;
-            device::threads.devices.erase_unordered(it);
-            y_always_assert(device::active_pools == device::threads.devices.size(), "Invalid number of pools");
+                *it->second = nullptr;
+                devices.erase_unordered(it);
+                y_always_assert(device::active_pools == devices.size(), "Invalid number of pools");
+            });
         }
     });
 
@@ -290,8 +289,9 @@ ThreadDevicePtr thread_device() {
         auto device = std::make_unique<ThreadLocalDevice>();
         thread_device = device.get();
 
-        const auto lock = y_profile_unique_lock(device::threads.lock);
-        device::threads.devices.emplace_back(std::move(device), &thread_device);
+        device::thread_devices.locked([&](auto&& devices) {
+            devices.emplace_back(std::move(device), &thread_device);
+        });
     }
 
     return thread_device;
