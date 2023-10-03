30050ae2263d01551970cd95345c293d148c1d7d
gan74 <gregoire.angerand@gmail.com>
2023-10-03 13:42:36 +0200

    Fixed safe de-serialization skipping poly classes

--- y/y/serde3/archives.h
+++ y/y/serde3/archives.h
@@ -729,6 +729,7 @@ class ReadableArchive final {
                 TypeId type_id;
                 y_try(read_one(type_id));
 
+                const bool propagate_poly_failure = (_flags & DeserializationFlags::DontPropagatePolyFailure) == DeserializationFlags::None;
                 if constexpr(is_std_ptr_v<T>) {
                     static_assert(!has_serde3_v<T>);
 
@@ -738,14 +739,18 @@ class ReadableArchive final {
                         object.object = poly_type->create();
                         if(object.object) {
                             auto r = object.object->_y_serde3_poly_deserialize(*this);
-                            if(r.is_ok() || (_flags & DeserializationFlags::DontPropagatePolyFailure) == DeserializationFlags::None) {
+                            if(r.is_ok() || propagate_poly_failure) {
                                 return r;
                             }
                             object.object = nullptr;
                         }
                     }
 
-                    return core::Ok(Success::Partial);
+                    if(propagate_poly_failure) {
+                        return core::Err(Error(ErrorType::UnknownPolyError, object.name.data()));
+                    } else {
+                        return core::Ok(Success::Partial);
+                    }
                 } else {
                     static_assert(has_serde3_poly_v<T>);
                     static_assert(!has_serde3_v<T>);
@@ -838,8 +843,7 @@ class ReadableArchive final {
         }
 
         template<bool Safe, usize I, typename T>
-        inline Result deserialize_members_internal(T& object,
-                                            ObjectData& object_data) {
+        inline Result deserialize_members_internal(T& object, ObjectData& object_data) {
             unused(object, object_data);
 
 #ifdef Y_NO_SAFE_DESER
@@ -849,8 +853,11 @@ class ReadableArchive final {
             const auto members = list_members<T>();
             if constexpr(I < std::tuple_size_v<decltype(members)>) {
                 auto member = std::get<I>(members).materialize(object);
-
                 if constexpr(Safe) {
+                    // Removed all flags when trying to deserialize members to ensure errors are never ignored
+                    const DeserializationFlags flags = std::exchange(_flags, DeserializationFlags::None);
+                    y_defer(_flags = flags);
+
                     auto& offsets = object_data.members_offsets;
                     if(offsets.is_empty()) {
                         return core::Ok(Success::Partial);
@@ -864,7 +871,7 @@ class ReadableArchive final {
                         y_try(read_one(size));
                         const usize end = tell() + size;
 
-                        if(deserialize_one(member)) {
+                        if(const auto res = deserialize_one(member); res.is_ok() && res.unwrap() == Success::Full) {
                             if(tell() != end) {
                                 return core::Err(Error(ErrorType::SignatureError, member.name.data()));
                             }
