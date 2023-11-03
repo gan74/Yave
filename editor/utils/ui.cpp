5f1458f74fe712cee894242ee9aa307f3aa119c7
gan74 <gregoire.angerand@gmail.com>
2023-11-04 00:29:35 +0100

    Fixed text input

--- editor/utils/ui.cpp
+++ editor/utils/ui.cpp
@@ -172,12 +172,12 @@ bool text_input(const char* name, core::String& str, ImGuiInputTextFlags flags) 
     y_defer(ImGui::PopStyleColor());
 
     y_defer(str.resize(std::strlen(str.data())));
-    return ImGui::InputText(name, str.data(), str.size(), flags | ImGuiInputTextFlags_CallbackResize, str_resize_callback, &str);
+    return ImGui::InputText(name, str.data(), str.size() + 1, flags | ImGuiInputTextFlags_CallbackResize, str_resize_callback, &str);
 }
 
 bool text_input_multiline(const char* name, core::String& str) {
     y_defer(str.resize(std::strlen(str.data())));
-    return ImGui::InputTextMultiline(name, str.data(), str.size(), ImVec2(), ImGuiInputTextFlags_CallbackResize, str_resize_callback, &str);
+    return ImGui::InputTextMultiline(name, str.data(), str.size() + 1, ImVec2(), ImGuiInputTextFlags_CallbackResize, str_resize_callback, &str);
 }
 
 void text_read_only(const char* name, std::string_view str) {
