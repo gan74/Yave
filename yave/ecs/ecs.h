0ab08099a079c32cdc5cfc1e7eb6e1ea6e6a0d8a
gan74 <gregoire.angerand@gmail.com>
2024-02-26 22:07:30 +0100

    Revert "Better ecs type_index"
    This reverts commit dbd0c517b1de051d5b3bfd125e73184341eef6f5.

--- yave/ecs/ecs.h
+++ yave/ecs/ecs.h
@@ -35,16 +35,14 @@ enum class ComponentTypeIndex : u32 {
 
 namespace detail {
 ComponentTypeIndex next_type_index();
-
-
-template<typename T>
-static ComponentTypeIndex type_index = next_type_index();
 }
 
 
 template<typename T>
 ComponentTypeIndex type_index() {
-    return detail::type_index<T>;
+    static_assert(!std::is_const_v<T> && !std::is_reference_v<T>);
+    static ComponentTypeIndex type(detail::next_type_index());
+    return type;
 }
 
 
