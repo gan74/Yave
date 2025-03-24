4f7d0c9dc277fc52a1f99460ef1368fc6c272ae9
gan74 <gregoire.angerand@gmail.com>
2025-03-24 12:08:14 +0100

    Removed jolt

--- external/JoltPhysics/Assets/Shaders/VK/TriangleDepthPixelShader.frag
+++
@@ -1,6 +0,0 @@
-#version 450
-
-// We only write depth, so this shader does nothing
-void main()
-{
-}
--- external/JoltPhysics/Build/cmake_xcode_ios.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cmake -S . -B XCode_iOS -DTARGET_HELLO_WORLD=OFF -DTARGET_PERFORMANCE_TEST=OFF -DCMAKE_SYSTEM_NAME=iOS -GXcode
-open XCode_iOS/JoltPhysics.xcodeproj
