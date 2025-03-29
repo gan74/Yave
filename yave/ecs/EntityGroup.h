12fb343c26a280fc86bc76b56c54588e3ad9f062
gan74 <gregoire.angerand@gmail.com>
2025-03-29 14:17:11 +0100

    Added ecs::AnyChanged

--- yave/ecs/EntityGroup.h
+++ yave/ecs/EntityGroup.h
@@ -169,6 +169,7 @@ class EntityGroup final : NonCopyable {
     static constexpr usize type_count = sizeof...(Ts);
     static constexpr usize mutate_count = ((traits::is_component_mutable_v<Ts> ? 1 : 0) + ...);
 
+    static constexpr bool has_any = (traits::is_component_filter_any_v<Ts> || ...);
     static constexpr usize changed_count = ((traits::is_component_changed_v<Ts> ? 1 : 0) + ...);
     static constexpr usize deleted_count = ((traits::is_component_deleted_v<Ts> ? 1 : 0) + ...);
     static constexpr usize filter_count = changed_count + deleted_count;
@@ -178,7 +179,7 @@ class EntityGroup final : NonCopyable {
     using ComponentTuple = std::tuple<traits::component_type_t<Ts>&...>;
 
     using MutateContainers = std::array<SparseIdSet*, mutate_count>;
-    using FilterContainers = std::array<const SparseIdSet*, filter_count>;
+    using FilterContainers = std::array<std::pair<const SparseIdSet*, bool>, filter_count>;
 
     template<typename T>
     static inline T& get_component(const SetTuple& sets, EntityId id) {
@@ -200,10 +201,16 @@ class EntityGroup final : NonCopyable {
         }
 
         if constexpr(traits::is_component_changed_v<T>) {
-            _filter[filter_index++] = &std::get<I>(containers)->_mutated;
+            _filter[filter_index++] = {
+                &std::get<I>(containers)->_mutated,
+                traits::is_component_filter_any_v<T>
+            };
         }
         if constexpr(traits::is_component_deleted_v<T>) {
-            _filter[filter_index++] = &std::get<I>(containers)->_to_delete;
+            _filter[filter_index++] = {
+                &std::get<I>(containers)->_to_delete,
+                traits::is_component_filter_any_v<T>
+            };
         }
     }
 
@@ -215,7 +222,7 @@ class EntityGroup final : NonCopyable {
         (fill_one<Ts, Is>(containers, mut_index, filter_index, const_index), ...);
 
         y_debug_assert(std::all_of(_mutate.begin(), _mutate.end(), [](const auto* s) { return s; }));
-        y_debug_assert(std::all_of(_filter.begin(), _filter.end(), [](const auto* s) { return s; }));
+        y_debug_assert(std::all_of(_filter.begin(), _filter.end(), [](auto s) { return s.first; }));
         y_debug_assert(std::all_of(_write_locks.begin(), _write_locks.end(), [](const auto* s) { return s; }));
         y_debug_assert(std::all_of(_read_locks.begin(), _read_locks.end(), [](const auto* s) { return s; }));
     }
@@ -348,24 +355,48 @@ class EntityGroup final : NonCopyable {
             if constexpr(filter_count) {
                 y_profile_zone("finding changed entities");
 
-                std::array<const SparseIdSet*, filter_count + 1> matches = {};
+                std::array<std::pair<const SparseIdSet*, bool>, filter_count + 1> matches = {};
                 std::copy_n(_filter.begin(), filter_count, matches.begin());
-                matches[filter_count] = &_provider->ids();
+                matches[filter_count] = {&_provider->ids(), false};
 
-                std::sort(matches.begin(), matches.end(), [](const SparseIdSet* a, const SparseIdSet* b) {
-                    return a->size() < b->size();
+                std::sort(matches.begin(), matches.end(), [](const auto& a, const auto& b) {
+                    if(a.second != b.second) {
+                        return b.second;
+                    }
+                    return a.first->size() < b.first->size();
                 });
 
-                _ids.set_min_capacity(matches[0]->ids().size());
+                y_debug_assert(!matches[0].second);
 
-                for(const EntityId id : matches[0]->ids()) {
+                const usize or_matches = std::count_if(matches.begin(), matches.end(), [](const auto& m) { return m.second; });
+                const usize and_matches = filter_count - or_matches + 1;
+                y_debug_assert(and_matches + or_matches == matches.size());
+                y_debug_assert(!!or_matches == has_any);
+
+                _ids.set_min_capacity(matches[0].first->ids().size());
+
+                for(const EntityId id : matches[0].first->ids()) {
                     bool match = true;
-                    for(usize i = 1; i != matches.size(); ++i) {
-                        if(!matches[i]->contains(id)) {
+                    for(usize i = 1; i != and_matches; ++i) {
+                        y_debug_assert(!matches[i].second);
+                        if(!matches[i].first->contains(id)) {
                             match = false;
                             break;
                         }
                     }
+                    if constexpr(has_any) {
+                        if(match) {
+                            match = false;
+                            for(usize i = and_matches; i != matches.size(); ++i) {
+                                y_debug_assert(matches[i].second);
+                                if(matches[i].first->contains(id)) {
+                                    match = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
                     if(match) {
                         _ids << id;
                     }
--- yave/ecs/traits.h
+++ yave/ecs/traits.h
@@ -37,6 +37,9 @@ template<typename T>
 struct Changed {};
 
 template<typename T>
+struct AnyChanged {};
+
+template<typename T>
 struct Deleted {};
 
 
@@ -49,6 +52,7 @@ struct component_type {
     using type = const raw_type;
 
     static constexpr bool changed = false;
+    static constexpr bool any = false;
     static constexpr bool deleted = false;
 };
 
@@ -60,6 +64,7 @@ struct component_type<Mutate<T>> {
     using type = std::remove_const_t<typename component_type<T>::type>;
 
     static constexpr bool changed = component_type<T>::changed;
+    static constexpr bool any = component_type<T>::any;
     static constexpr bool deleted = component_type<T>::deleted;
 };
 
@@ -71,9 +76,22 @@ struct component_type<Changed<T>> {
     using type = typename component_type<T>::type;
 
     static constexpr bool changed = true;
+    static constexpr bool any = false;
     static constexpr bool deleted = component_type<T>::deleted;
 };
 
+template<typename T>
+struct component_type<AnyChanged<T>> {
+        static_assert(!component_type<T>::deleted);
+        static_assert(!component_type<T>::changed);
+
+        using raw_type = typename component_type<T>::raw_type;
+        using type = typename component_type<T>::type;
+
+        static constexpr bool changed = true;
+        static constexpr bool any = true;
+        static constexpr bool deleted = component_type<T>::deleted;
+};
 
 template<typename T>
 struct component_type<Deleted<T>> {
@@ -84,6 +102,7 @@ struct component_type<Deleted<T>> {
     using type = typename component_type<T>::type;
 
     static constexpr bool changed = false;
+    static constexpr bool any = false;
     static constexpr bool deleted = true;
 };
 
@@ -100,6 +119,9 @@ using discard_query_qualifiers_t = typename component_type<T>::discard_query_qua
 
 template<typename T>
 static constexpr bool is_component_changed_v = component_type<T>::changed;
+
+template<typename T>
+static constexpr bool is_component_filter_any_v = component_type<T>::any;
 
 template<typename T>
 static constexpr bool is_component_const_v = std::is_const_v<typename component_type<T>::type>;
