2cd64728ad76b65285c07025cc83507746b41353
gan74 <gregoire.angerand@gmail.com>
2026-08-26 21:05:39 +0200

    Fixed validation error on startup

--- yave/graphics/swapchain/Swapchain.cpp
+++ yave/graphics/swapchain/Swapchain.cpp
@@ -282,12 +282,6 @@ bool Swapchain::build_swapchain() {
         _images << std::move(swapchain_image);
     }
 
-    CmdBufferRecorder recorder = create_disposable_cmd_buffer();
-    for(auto& i : _images) {
-        recorder.barriers({ImageBarrier::transition_barrier(i, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)});
-    }
-    recorder.submit().wait();
-
     return true;
 }
 
