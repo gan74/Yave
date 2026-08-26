db04ea5b73dc3ccd58306b8a025b67dfbf2a7a83
gan74 <gregoire.angerand@gmail.com>
2026-08-26 21:05:39 +0200

    Fixed validation error on startup

--- yave/graphics/framebuffer/RenderPass.cpp
+++ yave/graphics/framebuffer/RenderPass.cpp
@@ -76,25 +76,20 @@ static std::array<VkSubpassDependency, 2> create_dependencies(bool depth, bool c
     return {top, bot};
 }
 
-static VkAttachmentLoadOp attachment_load_op(const RenderPass::AttachmentData& image) {
-    return image.load_op == RenderPass::LoadOp::Clear
-        ? VK_ATTACHMENT_LOAD_OP_CLEAR
-        : VK_ATTACHMENT_LOAD_OP_LOAD;
-}
-
 static VkAttachmentDescription create_attachment(RenderPass::AttachmentData image) {
     VkAttachmentDescription description = {};
     {
+        const bool clear_image = (image.load_op == RenderPass::LoadOp::Clear);
         const auto layout = vk_final_image_layout(image.usage);
 
         description.format = image.format.vk_format();
         description.samples = VK_SAMPLE_COUNT_1_BIT;
-        description.loadOp = attachment_load_op(image);
+        description.loadOp = clear_image ? VK_ATTACHMENT_LOAD_OP_CLEAR :VK_ATTACHMENT_LOAD_OP_LOAD;
         description.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
         description.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
         description.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
         description.finalLayout = layout;
-        description.initialLayout = layout;
+        description.initialLayout = clear_image ? VK_IMAGE_LAYOUT_UNDEFINED : layout;
     }
     return description;
 }
